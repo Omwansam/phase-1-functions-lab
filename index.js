06f3ef8ee9e77bec3f4fca2804c82fa7072ea74f
Omwansam <omwansamarnold@gmail.com>
2024-07-07 08:35:20 +0300

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(picklocation) {
+    const HqInBlocks = 42
+    return Math.abs(picklocation-42)
+}   
+
+function distanceFromHqInFeet(picklocation) {
+    const Blocks = distanceFromHqInBlocks(picklocation);
+    return Blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if(distanceInFeet <= 400) {
+    return 0;
+    } else if (distanceInFeet <= 2000){
+         return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+         return 25;
+    } else {
+         return 'cannot travel that far';
+    }
+}
